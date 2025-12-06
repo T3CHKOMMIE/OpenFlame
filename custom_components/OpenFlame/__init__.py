bb061b3806271b18dd924c346a7972029c5f7cc2
T3CHKOMMIE <T3chk0mm!e>
2025-12-05 18:57:56 -0600

    more nonsense

--- custom_components/OpenFlame/__init__.py
+++ custom_components/OpenFlame/__init__.py
@@ -1,7 +1,16 @@
-"""The OpenFlame HACS Integration."""
-DOMAIN = "OpenFlame"
+"""The OpenFlame Integration."""
+import logging
 
-async def async_setup(hass, config):
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "openflame"
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the OpenFlame component."""
-    # Add any global setup here if needed
+    _LOGGER.info("OpenFlame is setting up!")
+    # Store configuration or perform other setup tasks here
+    hass.data[DOMAIN] = {} # Example: store data for your component
     return True
--- custom_components/OpenFlame/switch.py
+++ custom_components/OpenFlame/switch.py
@@ -1,28 +1,44 @@
-"""Platform for OpenFlame HACS Integration."""
+"""Platform for switch integration."""
 from homeassistant.components.switch import SwitchEntity
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+
+async def async_setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType = None,
+) -> None:
+    """Set up the My Simple Switch platform."""
+    # Add your switch entities here
+    add_entities([MySimpleSwitch(hass, "OpenFlame")])
 
 class MySimpleSwitch(SwitchEntity):
-    """Representation of a My Simple Switch."""
+    """Representation of a simple switch."""
 
-    def __init__(self, name):
+    def __init__(self, hass: HomeAssistant, name: str) -> None:
         """Initialize the switch."""
+        self._hass = hass
         self._name = name
-        self._state = False
+        self._state = False  # Initial state of the switch
 
     @property
-    def name(self):
+    def name(self) -> str:
         """Return the name of the switch."""
         return self._name
 
     @property
-    def is_on(self):
+    def is_on(self) -> bool:
         """Return true if switch is on."""
         return self._state
 
-    async def async_turn_on(self, **kwargs):
+    async def async_turn_on(self, **kwargs) -> None:
         """Turn the switch on."""
         self._state = True
+        self.async_write_ha_state() # Notify Home Assistant of state change
 
-    async def async_turn_off(self, **kwargs):
+    async def async_turn_off(self, **kwargs) -> None:
         """Turn the switch off."""
         self._state = False
+        self.async_write_ha_state() # Notify Home Assistant of state change
